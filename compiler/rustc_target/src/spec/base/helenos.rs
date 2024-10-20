edea3c4c9d3d6f50dc02ad0ae77160f9afc746fc
Matěj Volf <git@mvolfik.com>
2024-10-20 15:22:27 +0200

    inital HelenOS support

---
+++ compiler/rustc_target/src/spec/base/helenos.rs
@@ -0,0 +1,21 @@
+use crate::spec::{cvs, LinkerFlavor, Cc, Lld, RelroLevel, TargetOptions};
+
+pub fn opts() -> TargetOptions {
+    TargetOptions {
+        os: "helenos".into(),
+        dynamic_linking: true,
+        families: cvs!["unix"],
+        crt_static_default: true,
+        crt_static_allows_dylibs: true,
+        has_thread_local: true,
+        has_rpath: true,
+        relro_level: RelroLevel::Full,
+        pre_link_args: TargetOptions::link_args(
+            LinkerFlavor::Gnu(Cc::No, Lld::No),
+            &[
+                "--hash-style=sysv", "-m64"
+            ],
+        ),
+        ..Default::default()
+    }
+}
